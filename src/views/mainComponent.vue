0bdb54df8567517d3ac6a2b2d6e90e87b040ec2d
seniya2 <seniya2@gmail.com>
2020-10-25 17:29:43 +0900

    진행중

--- src/views/mainComponent.vue
+++ src/views/mainComponent.vue
@@ -10,29 +10,62 @@
         :load-children="onSelectedFolder"
         :active.sync="active"
         :open.sync="open"
-        >
+        @update:active="onUpdateActiveFolder">
+          <template v-slot:prepend="{ item, open }">
+            <v-icon v-if="item.nodeKey === 'ROOT'" medium color="blue darken-3">
+              mdi-desktop-classic
+            </v-icon>
+            <v-icon v-else medium color="blue darken-3">
+              {{open ? 'mdi-folder-open' : 'mdi-folder'}}
+            </v-icon>
+          </template>
           <template v-slot:label="{ item }">
-            <a @dblclick="onDbSelectedFolder(item)">{{item.label}}</a>
-            <!-- <v-btn @click="onSelectedFolder(item, item.nodeKey)">{{item.label}}</v-btn> -->
-            <!-- <v-btn >{{item.label}}</v-btn> -->
+            <span class="folder-label-text">{{item.label}}</span>
           </template>
         </v-treeview>
 
     </div>
     <div ref="listBoxHandler" class="list-box-handler" @mousedown="onHandleColumsDown"></div>
     <div ref="filesBox" class="list-box files-box">
-      <v-list-item v-for="(item, index) in contents" :key="index + '_' + item.id">
-        <v-list-item-content>
-          <v-list-item-title>{{item.name}}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
+      <v-data-table
+        :headers="headers"
+        :items="contents"
+        hide-default-footer
+        :items-per-page="contents.length"
+        item-key="nodeKey"
+        class="elevation-1">
+        <template v-slot:body="{ items }">
+          <tbody>
+            <tr v-for="item in items" :key="item.label">
+              <td>
+                <v-icon medium left>{{getFileIcon(item.data)}}</v-icon>
+                <span>{{item.label}}</span>
+              </td>
+              <td>
+                <span v-if="!item.data.isDir">{{getFileType(item.label)}}</span>
+                <span v-else>폴더</span>
+              </td>
+              <td>
+                <span>
+                  {{getFileTime(item.data.stat.birthtimeMs)}}
+                </span>
+              </td>
+              <td>
+                <span v-if="!item.data.isDir">{{getFileSizeIEC(item.data.stat.size)}}</span>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </v-data-table>
     </div>
   </div>
 </template>
 
 <script>
 import { ipcRenderer } from 'electron'
+import ICON_TYPE from '@/util/iconType'
 import _ from 'lodash'
+import moment from 'moment'
 const path = require('path')
 const fs = require('fs')
 const mime = require('mime-types')
@@ -43,6 +76,8 @@ window.ipcRenderer = ipcRenderer
 function * walkFolders (folder, recurseLevel = 0) {
   try {
     const files = fs.readdirSync(folder)
+    // console.log('walkFolders folder : ', folder)
+    // console.log('walkFolders files : ', files)
 
     for (const file of files) {
       try {
@@ -125,16 +160,32 @@ export default {
     this.onHandleColumsInit()
   },
 
-  watch: {
-    active (newValue, oldValue) {
-      if (newValue.length === 0 && oldValue.length === 1) {
-        newValue.push(oldValue[0])
-      }
-    },
-    open (newValue, oldValue) {
-      console.log('open newValue : ', newValue)
-      console.log('open oldValue : ', oldValue)
+  computed: {
+    headers () {
+      return [
+        {
+          text: '이름',
+          align: 'start',
+          sortable: true,
+          value: 'label'
+        },
+        { text: '타입', value: 'data.mimeType' },
+        { text: '날짜', value: 'data.stat.birthtimeMs' },
+        { text: '크기', value: 'data.stat.size' }
+      ]
     }
+  },
+
+  watch: {
+    // active (newValue, oldValue) {
+    //   if (newValue.length === 0 && oldValue.length === 1) {
+    //     newValue.push(oldValue[0])
+    //   }
+    // },
+    // open (newValue, oldValue) {
+    //   console.log('open newValue : ', newValue)
+    //   console.log('open oldValue : ', oldValue)
+    // }
   },
 
   methods: {
@@ -167,6 +218,25 @@ export default {
       this.contents.splice(0, this.contents.length)
     },
 
+    onUpdateActiveFolder (items) {
+      console.log('onUpdateActiveFolder 시작 items[0] : ', items[0])
+      console.log('onUpdateActiveFolder 시작 this.selectedFolder : ', this.selectedFolder)
+      // if (items.length === 0 && this.selectedFolder !== null) {
+      //   if (this.selectedFolder !== undefined) {
+      //     this.active = [this.selectedFolder]
+      //   }
+      // }
+      if (this.selectedFolder !== this.active[0]) {
+        console.log('onUpdateActiveFolder 달라요 : ', items[0])
+        this.selectedFolder = items[0] + path.sep
+        this.clearAllContentItems()
+        const newContents = this.getFolderContents(this.selectedFolder)
+        console.log('newContents : ', newContents)
+        this.contents.push(...newContents)
+      }
+      // console.log('onUpdateActiveFolder items : ', items)
+    },
+
     onSelectedFolder (item) {
       // console.log('onSelectedFolder item : ', item)
       // console.log('onSelectedFolder active: ', this.active)
@@ -177,19 +247,20 @@ export default {
 
     onDbSelectedFolder (item) {
       console.log('onDbSelectedFolder item : ', item)
-      this.open.push(item.id)
+      // this.open.push(item.id)
     },
 
     setSelectedFolder (item, folder) {
-      this.selectedFolder = folder
-      if (process.platform === 'win32') {
-        if (this.selectedFolder.charAt(this.selectedFolder.length - 1) === ':') {
-          this.selectedFolder += path.sep
-        }
-      }
-      this.loadChildren(item, this.selectedFolder)
+      // this.selectedFolder = folder
+      // if (process.platform === 'win32') {
+      //   if (this.selectedFolder.charAt(this.selectedFolder.length - 1) === ':') {
+      //     this.selectedFolder += path.sep
+      //   }
+      // }
+      folder += path.sep
+      this.getFolders(item, folder)
     },
-    loadChildren (node, key) {
+    getFolders (node, key) {
       try {
         if (node.children.length) {
           node.children.splice(0, node.children.length)
@@ -197,7 +268,7 @@ export default {
 
         // const logs = walkFolders(key, 0)
         // console.log('node : ', node)
-        console.log('key : ', key)
+        // console.log('loadChildren key : ', key)
 
         for (const fileInfo of walkFolders(key, 0)) {
           // console.log('fileInfo : ', fileInfo)
@@ -208,7 +279,7 @@ export default {
           const n = this.createNode(fileInfo)
           node.children.push(n)
         }
-        console.log('node : ', _.cloneDeep(node))
+        console.log('loadChildren node : ', _.cloneDeep(node))
         return true
       } catch (err) {
         // usually access error
@@ -218,6 +289,7 @@ export default {
     },
 
     getFolderContents (folder) {
+      console.log('getFolderContents folder : ', folder)
       const contents = []
 
       // check incoming arg
@@ -225,6 +297,8 @@ export default {
         return contents
       }
 
+      let newFolders = []
+      let newFiles = []
       for (const fileInfo of walkFolders(folder, 0)) {
         // all files and folders
         if ('error' in fileInfo) {
@@ -232,9 +306,14 @@ export default {
           continue
         }
         const node = this.createNode(fileInfo)
-        contents.push(node)
+        if (node.data.isDir) newFolders.push(node)
+        if (!node.data.isDir) newFiles.push(node)
       }
 
+      newFolders = _.orderBy(newFolders, ['label'], ['asc'])
+      newFiles = _.orderBy(newFiles, ['label'], ['asc'])
+
+      contents.push(...newFolders, ...newFiles)
       return contents
     },
 
@@ -267,13 +346,36 @@ export default {
           stat: fileInfo.stat
         }
       }
-    }
+    },
 
+    getFileIcon (itemData) {
+      // const returnValue = `(${itemData.mimeType})`
+      let vIco = ICON_TYPE[itemData.mimeType]
+      if (itemData.isDir) vIco = 'mdi-folder'
+      if (vIco === undefined) vIco = 'mdi-file'
+      // console.log(`mimeType: ${itemData.mimeType} / vIco: ${vIco}`)
+      return vIco
+    },
+
+    getFileSizeIEC (a, b, c, d, e) {
+      // console.log('fileSizeIEC a: ', a)
+      b = Math
+      c = b.log
+      d = 1024
+      e = c(a) / c(d) | 0
+      return (a / b.pow(d, e)).toFixed(2) + ' ' + (e ? 'kMGTPEZY'[--e] + 'B' : 'Bytes')
+    },
+    getFileType (mimeType) {
+      return mimeType.split('.').pop()
+    },
+    getFileTime (time) {
+      return moment(this.time).format('YYYY-MM-DD HH:mm')
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scope>
 
 .main-wrapper {
   position: relative;
@@ -283,7 +385,7 @@ export default {
   min-height: 100%;
 
   .list-box-handler {
-    width: 2px;
+    width: 5px;
     padding: 0;
     cursor: ew-resize;
     flex: 0 0 auto;
@@ -304,20 +406,33 @@ export default {
     .list-box-handler::before {
       content: '';
       display: block;
-      width: 1px;
+      width: 5px;
       height: 100%;
       background: #999;
       margin: 0 auto;
     }
-
-    .folders-box {
-      overflow-y: scroll;
-    }
-
-    .files-box {
-      overflow: scroll;
-    }
   }
+
+  .folders-box {
+    overflow-y: scroll;
+  }
+
+  .files-box {
+    overflow: scroll;
+  }
+
+  .folder-label-text{
+    font-size: 0.9rem;
+  }
+
+  .v-treeview-node__root {
+    margin-top: 0px !important;
+    margin-bottom: 0px !important;
+  }
+
 }
 
+// $treeview-node-margin : 0px !default;
+// $treeview-node-shaped-margin: 0px !default;
+// $treeview-node-padding: 0px !default;
 </style>
